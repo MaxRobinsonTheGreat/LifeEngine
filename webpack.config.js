8fb471025e7ef11490432a342f1694624b1f39ee
MaxRobinsonTheGreat <maxdrob1@gmail.com>
2020-07-22 18:52:51 -0600

    changed project structure for hosting

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'dist/js/'),
     },
     externals: {
         jquery: 'jQuery'
