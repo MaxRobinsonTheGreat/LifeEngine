cafb444417a14d1ca0ca5ca71933b90a122210d2
MaxRobinsonTheGreat <maxdrob1@gmail.com>
2020-07-23 15:52:02 -0600

    Mobile defense/firefox pixelation

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@
 import Engine from './Engine';
 
 $('document').ready(function(){
+    let isMobile = window.matchMedia("only screen and (max-width: 760px)").matches;
+
+    console.log(isMobile)
+    if (isMobile) {
+        alert("Though the simulation still works on mobile, most features are disabled. Try it on desktop for the full experience!");
+        $('.control-panel').css('display', 'none');
+    }
     var engine = new Engine();
     engine.start(60);
 });
