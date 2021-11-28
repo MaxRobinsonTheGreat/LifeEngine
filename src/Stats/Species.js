7e1a8bb2c2dfe18e548231a8e9d0d28b69cf7498
Max Robinson <maxdrob1@gmail.com>
2021-11-28 14:46:55 -0600

    removed ancestor tracking

--- src/Stats/Species.js
+++ src/Stats/Species.js
@@ -3,17 +3,11 @@ const CellStates = require("../Organism/Cell/CellStates");
 class Species {
     constructor(anatomy, ancestor, start_tick) {
         this.anatomy = anatomy;
-        this.ancestor = ancestor;
+        // this.ancestor = ancestor; // garbage collect ancestors to avoid memory problems
         this.population = 1;
         this.cumulative_pop = 1;
         this.start_tick = start_tick;
         this.end_tick = -1;
-        this.color = Math.floor(Math.random()*16777215).toString(16);
-        if (ancestor != null) {
-            // needs to be reworked, maybe removed
-            var mutator = Math.floor(Math.random()*16777215)-8000000;
-            this.color = (mutator + parseInt(ancestor.color, 16)).toString(16);
-        }
         this.name = '_' + Math.random().toString(36).substr(2, 9);
         this.extinct = false;
         this.calcAnatomyDetails();
