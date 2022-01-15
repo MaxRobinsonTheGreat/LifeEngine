f1722d65c6da0134aa2d3d1e0eaebbaf8cb3ecaa
showierdata9978 <68120127+showierdata9978@users.noreply.github.com>
2022-01-15 16:06:57 -0600

    Create HealCell.js
    Bad Code :D (I'm New To js )
    first timer writes bad  JS

---
+++ src/Organism/Cell/BodyCells/HealCell.js
@@ -0,0 +1,28 @@
+const CellStates = require("../CellStates");
+const BodyCell = require("./BodyCell");
+const Brain = require("./BrainCell.js);
+const hyperperams = require('./Hyperparameters.js);
+
+class HealCell extends BodyCell{
+                      constructor(org, loc_col, loc_row){
+                        super(CellStates.HealCell,org,loc_col,loc_row)
+                        if 
+                          this.org.anatomy.Can_Heal = True
+                      }
+                      performFunction() {
+                         var env = this.org.env;
+                         var Helth = this.damage 
+                         if Helth > 0 {
+                         Heal(this);
+                         }
+                       heal(this){
+                         if this.food_collected > 2 {
+                          this.damage = this.damage - 1;
+                          this.food_collected = this.food_collected - 2 ;
+                         }
+                       }
+                         
+                      
+                      
+                      }
+module.exports = HealCell;
